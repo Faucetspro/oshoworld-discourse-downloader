63edcbf5ec0f8ba95c274ecca2dcb96d0d7109f3
DharmendraKumar_Gupt <dharmendrakumar_gupt@symantec.com>
2020-03-14 01:19:28 +0530

    Fix

--- index.js
+++ index.js
@@ -65,8 +65,26 @@ const downloadFiles = async function(){
             fs.mkdirSync(downloadPath);
         }
         const fileName = path.basename(url).slice(5);
-        const dl = downloader.download(url, downloadPath +fileName).start();
+        const dl = downloader.download(url, downloadPath +fileName);
+        // Set retry options
+        dl.setRetryOptions({
+            maxRetries: 10,		// Default: 5
+            retryInterval: 4000 // Default: 2000
+        });
+
+        // Set download options
+        dl.setOptions({
+            threadsCount: 1, // Default: 2, Set the total number of download threads
+            method: 'GET', 	 // Default: GET, HTTP method
+            port: 80, 	     // Default: 80, HTTP port
+            timeout: 10000,   // Default: 5000, If no data is received, the download times out (milliseconds)
+            range: '0-100',  // Default: 0-100, Control the part of file that needs to be downloaded.
+        });
         registerDlEvents(fileName, dl);
+        dl.start();
+        dl.on('start', function() {
+            console.log('Download started with '+ ((dl.meta.threads) ? dl.meta.threads.length : 0) +' threads.')
+        });
     }
 }
 
